f7d5bd061fc70abdb1e583605c2b16c5e8408b7f
ilgar-mammadyarov <ilqar.mammadyarov@gmail.com>
2024-05-23 19:34:47 +0400

    vite config base url changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deploying-rick-and-morty-vue/",
+  base: "/rick-and-morty-vue/",
   plugins: [vue()],
 });
